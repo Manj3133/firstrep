a60967451d29149e784254c80aaf5ef46f1310a8
Manj3133 <101574422+Manj3133@users.noreply.github.com>
2022-04-09 10:30:41 +0530

    added h1 from global repository

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Created local repository</h1>
       <h1>Linked local repository to global repository</h1>
+    <h1>Creating another heading from global repository<h1>
     </div>
   );
 }
